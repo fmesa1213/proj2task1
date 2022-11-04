f86457d00bdf0a9428a4a9ce814ee2b7ad496d08
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-03 21:32:47 -0400

    Create transcriptase.cpp

---
+++ transcriptase.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 2 Task A
+I wrote a program that reads a text file called dna.txt that contains one DNA strand per line
+*/
+
