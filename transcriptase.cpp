a967accbd8cf47cac2c250a676c5a19640855878
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-17 22:18:36 -0500

    Update transcriptase.cpp

--- transcriptase.cpp
+++ transcriptase.cpp
@@ -6,23 +6,29 @@ Assignment: Project 2 Task A
 I wrote a program that reads a text file called dna.txt that contains one DNA strand per line
 */
 
+
+#include <dna.txt>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <dna.txt>
+
 using namespace std;
 
 char DNAbase_to_mRNAbase(char dna){
 
 if(toupper(dna)=='A') 
 return 'U';
-else if(toupper(dna)=='T')
+
+else if(toupper(dna)=='T') 
 return 'A';
+
 else if(toupper(dna)=='C') 
 return 'G';
-else if(toupper(dna)=='G') 
+
+else if(toupper(dna)=='G')
 return 'C';
-else 
+
+else
 return ' ';
 }
 
@@ -34,6 +40,7 @@ output=output+DNAbase_to_mRNAbase(input[i]);
 }
 return output;
 }
+
 int main(){
 ifstream fin("dna.txt");
 string strand;
@@ -41,7 +48,6 @@ if (fin.fail()) {
 cerr << "File cannot be read, opened, or does not exist.\n";
 exit(1);
 }
-
 while(getline(fin, strand)) {
 cout << DNA_to_mRNA(strand) << endl; 
 }
